258f5cd719ad68ac23e9e7965676be30eda95e9f
Jeong Han Lee <jeonghan.lee@gmail.com>
2016-09-27 11:16:53 +0200

    clean up old env, and add epics-Apps

---
+++ epics-Apps/ee_setup.bash
@@ -0,0 +1,189 @@
+#!/bin/bash
+#
+#  Copyright (c) 2016 Jeong Han Lee
+#  Copyright (c) 2016 European Spallation Source ERIC
+#
+#  The ee_setup.bash is free software: you can redistribute
+#  it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 2 of the
+#  License, or any newer version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  this program. If not, see https://www.gnu.org/licenses/gpl-2.0.txt
+#
+# Shell  : ee_setup.bash
+# Author : Jeong Han Lee
+# email  : jeonghan.lee@gmail.com
+# Date   : 
+# version : 0.1.0 
+#
+# http://www.gnu.org/software/bash/manual/bashref.html#Bash-Builtins
+
+
+# 
+# PREFIX : SC_, so declare -p can show them in a place
+# 
+# Generic : Global vaiables - readonly
+#
+declare -gr SC_SCRIPT="$(realpath "$0")"
+declare -gr SC_SCRIPTNAME="$(basename "$SC_SCRIPT")"
+declare -gr SC_TOP="$(dirname "$SC_SCRIPT")"
+declare -gr SC_LOGDATE="$(date +%Y%b%d-%H%M-%S%Z)"
+
+
+# Generic : Redefine pushd and popd to reduce their output messages
+# 
+function pushd() { builtin pushd "$@" > /dev/null; }
+function popd()  { builtin popd  "$@" > /dev/null; }
+
+
+function ini_func() { printf "\n>>>> You are entering in : %s\n" "${1}"; }
+function end_func() { printf "<<<< You are leaving from %s\n" "${1}"; }
+
+function checkstr() {
+    if [ -z "$1" ]; then
+	printf "%s : input variable is not defined \n" "${FUNCNAME[*]}"
+	exit 1;
+    fi
+}
+
+# Generic : git_clone
+#
+# Required Global Variable
+# - SC_LOGDATE      : Input
+# Required Input Variagles
+# - ${1} : git source top directory
+# - ${2} : git source url
+# - ${3} : git source name
+
+function git_clone() {
+
+    local func_name=${FUNCNAME[*]}
+    local git_src_top=${1}
+    local git_src_url=${2}
+    local git_src_name=${3}
+    local git_src_dir=${git_src_top}/${git_src_name}
+    
+    ini_func ${func_name}
+    checkstr ${SC_LOGDATE}
+       
+    if [[ ! -d ${git_src_dir} ]]; then
+	echo "No git source repository in the expected location ${git_src_dir}"
+    else
+	echo "Old git source repository in the expected location ${git_src_dir}"
+	echo "The old one is renamed to ${git_src_dir}_${SC_LOGDATE}"
+	mv  ${git_src_dir} ${git_src_dir}_${SC_LOGDATE}
+    fi
+    
+    # Alwasy fresh cloning ..... in order to workaround any local 
+    # modification in the repository, which was cloned before. 
+    #
+    git clone ${git_src_url}/${git_src_name} ${git_src_top}/${git_src_name}
+    
+
+    end_func ${func_name}
+}
+
+# Generic : git_selection
+#
+# Require Global vairable
+# - SC_SELECTED_GIT_SRC  : Output
+#
+function git_selection() {
+
+    local func_name=${FUNCNAME[*]}
+    ini_func ${func_name}
+    
+    local git_ckoutcmd=""
+    local checked_git_src=""
+    declare -i index=0
+    declare -i master_index=0
+    declare -i list_size=0
+    declare -i selected_one=0
+    declare -a git_src_list=()
+
+
+    git_src_list+=("master")
+    git_src_list+=($(git tag -l | sort -n))
+    
+    for tag in "${git_src_list[@]}"
+    do
+	printf "%2s: git src %34s\n" "$index" "$tag"
+	let "index = $index + 1"
+    done
+    
+    echo -n "Select master or one of tags which can be built, followed by [ENTER]:"
+
+    # don't wait for 3 characters 
+    # read -e -n 2 line
+    read -e line
+   
+    # convert a string to an integer?
+    # do I need this? 
+    # selected_one=${line/.*}
+
+    selected_one=${line}
+
+    let "list_size = ${#git_src_list[@]} - 1"
+    
+    if [[ "$selected_one" -gt "$list_size" ]]; then
+	printf "\n>>> Please select one number smaller than %s\n" "${list_size}"
+	exit 1;
+    fi
+    if [[ "$selected_one" -lt 0 ]]; then
+	printf "\n>>> Please select one number larger than 0\n" 
+	exit 1;
+    fi
+
+    SC_SELECTED_GIT_SRC="$(tr -d ' ' <<< ${git_src_list[line]})"
+    
+    printf "\n>>> Selected %34s --- \n" "${SC_SELECTED_GIT_SRC}"
+ 
+    echo ""
+    if [ "$selected_one" -ne "$master_index" ]; then
+	git_ckoutcmd="git checkout tags/${SC_SELECTED_GIT_SRC}"
+	$git_ckoutcmd
+	checked_git_src="$(git describe --exact-match --tags)"
+	checked_git_src="$(tr -d ' ' <<< ${checked_git_src})"
+	
+	printf "\n>>> Selected : %s --- \n>>> Checkout : %s --- \n" "${SC_SELECTED_GIT_SRC}" "${checked_git_src}"
+	
+	if [ "${SC_SELECTED_GIT_SRC}" != "${checked_git_src}" ]; then
+	    echo "Something is not right, please check your git reposiotry"
+	    exit 1
+	fi
+    else
+	git_ckoutcmd="git checkout ${SC_SELECTED_GIT_SRC}"
+	$git_ckoutcmd
+    fi
+    end_func ${func_name}
+ 
+}
+
+
+function git_select() {
+
+    SRC_NAME=${2}
+    #"danfysik-mps8500"
+    SRC_URL=${1}
+    # "https://github.com/jeonghanlee"
+    SRC_DIR=${SC_TOP}/${SC_GIT_SRC_NAME}
+
+
+    git_clone ${SRC_DIR} ${SRC_URL} ${SRC_NAME}
+
+    pushd ${SRC_DIR}
+    git_selection
+    popd
+    
+}
+
+git_select "https://github.com/jeonghanlee" "danfysik-mps8500"
+
+exit
+
